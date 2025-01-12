27d8cf82054253e07d3594d4131403fd06d0367e
NSandEP <starart2004@mail.ru>
2025-01-12 20:10:10 +0300

    Add files via upload

--- Module_3_5.py
+++ Module_3_5.py
@@ -3,8 +3,9 @@ def get_multiplied_digits(number):       #Создание функции get_mu
     first = int(str_number[0])           #Cоздание переменной first и запись в неё первого символа из str_number в числовом представлении(int)
     if len(str_number) > 1:              #Условие
         return first * get_multiplied_digits(int(str_number[1:]))        #Возвращаем значение
-    else:
-        return first           #Если же длина str_number не больше 1, тогда вернуть оставшуюся цифру first
+    elif int(str_number[1:]) != 0:
+        return first
+
 
 result = get_multiplied_digits(40203)    #Ввод значения
 print(result)                            #Вывод результата
